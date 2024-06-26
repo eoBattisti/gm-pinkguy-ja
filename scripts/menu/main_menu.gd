6def8c385ec5332774b19123114ed98186f3943a
eoBattisti <nickgabrielbd@gmail.com>
2024-06-26 17:45:13 -0300

    feat: add menus music

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	GameManager.start_game()
 
 func _on_credits_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/credits_menu.tscn")
--- scripts/singletons/game_manager.gd
+++ scripts/singletons/game_manager.gd
@@ -1,1 +1,9 @@
 extends Node
+
+
+func start_game() -> void:
+	# Handle all the game start logic
+	
+	# Stops the menu scene
+	Music.stop()
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
