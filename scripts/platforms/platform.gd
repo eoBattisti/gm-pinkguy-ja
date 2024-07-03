08044a2ce0282fabc9838e7992d2730d71aca301
eoBattisti <nickgabrielbd@gmail.com>
2024-07-03 18:03:49 -0300

    feat: updating game level

--- scripts/platforms/platform.gd
+++ scripts/platforms/platform.gd
@@ -1,15 +1,20 @@
 extends Node2D
 class_name Platform
 
+enum PlatformType { GRASS, RED_SAND, SAND, ICE }
+
 
 @export var movable: bool = false # Determines if a platform can move or not
 @export var offset: Vector2 = Vector2(0, -320)
 @export var duration: float = 5.0
+@export var type: PlatformType = PlatformType.GRASS
 
+@onready var sprite_2d: Sprite2D = $AnimatableBody2D/Sprite2D
 @onready var animatable_body_2d: AnimatableBody2D = $AnimatableBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	sprite_2d.frame = type
 	if movable:
 		_start_tween()
 
